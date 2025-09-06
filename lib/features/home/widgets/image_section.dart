802ab18071ef64f4f90c9f150e7035bcf6be983e
Osama Saeid <osamasaeed283@gmail.com>
2025-09-07 00:09:09 +0300

    feat: Replace hardcoded image URLs with AppConsts.images in ImagesSection

--- lib/features/home/widgets/image_section.dart
+++ lib/features/home/widgets/image_section.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:fci_rovers_app/core/consts/app_consts.dart';
 import 'package:fci_rovers_app/core/widgets/custom_title_widget.dart';
 import 'package:fci_rovers_app/core/widgets/custom_slider.dart';
 import 'package:fci_rovers_app/features/home/widgets/error_card.dart';
@@ -21,11 +22,7 @@ class ImagesSection extends StatelessWidget {
         const CustomTitleWidget(title: 'معرض الصور'),
         SizedBox(height: isMobile ? 24 : 45),
         CustomSlider(
-          items: [
-            'https://static.vecteezy.com/system/resources/previews/005/741/500/non_2x/happy-girl-scouts-set-collection-free-vector.jpg',
-            'https://static.vecteezy.com/system/resources/previews/005/741/500/non_2x/happy-girl-scouts-set-collection-free-vector.jpg',
-            'https://static.vecteezy.com/system/resources/previews/005/741/500/non_2x/happy-girl-scouts-set-collection-free-vector.jpg',
-          ],
+          items: AppConsts.images,
           itemBuilder: (context, item, index) {
             return ClipRRect(
               borderRadius: BorderRadius.circular(16),
@@ -37,7 +34,7 @@ class ImagesSection extends StatelessWidget {
                     borderRadius: BorderRadius.circular(8),
                     child: CachedNetworkImage(
                       imageUrl: item,
-                      fit: BoxFit.contain,
+                      fit: BoxFit.cover,
                       placeholder: (context, url) => LoadingCard(),
                       errorWidget: (context, url, error) => ErrorCard(),
                     ),
